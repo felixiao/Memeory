fb663257c8e9eb4925dc99c294b8ae04b179c35a
Felix <git@felixfei.com>
2015-06-09 23:51:36 +0800

    lala

---
+++ Memeory/Assets/Scripts/CardTouch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CardTouch : MonoBehaviour {
+    RectTransform rectTrans;
+    public Rect RectHit;
+	// Use this for initialization
+	void Start () {
+        rectTrans = GetComponent<RectTransform>();
+        RectHit = new Rect(transform.position.x - rectTrans.rect.width / 2, transform.position.y - rectTrans.rect.height / 2, transform.position.x + rectTrans.rect.width / 2, transform.position.y + rectTrans.rect.height / 2);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && HitTest(RectHit, Input.mousePosition))
+        {
+            Debug.Log("Hit " + this.name);
+        }
+        
+	}
+    public bool HitTest(Rect rect, Vector3 pos)
+    {
+        if (pos.x < rect.xMax && pos.x > rect.xMin && pos.y < rect.yMax && pos.y > rect.yMin)
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Memeory/Assets/Scripts/NumAnimation.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NumAnimation : MonoBehaviour {
+    public int curNum,targetNum,deltaNum;
+    public float MaxDuration;
+    bool playing=false;
+    int changePerDt;
+    Text text_num;
+	// Use this for initialization
+	void Start () {
+        curNum = 0;
+        text_num = GetComponent<Text>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playing)
+        {
+            OnPlaying();
+        }
+	}
+    void Reset()
+    {
+        curNum = 0;
+    }
+    public void OnPlaying()
+    {
+        curNum += changePerDt;
+        
+        if (curNum >= targetNum)
+        {
+            curNum = targetNum;
+            OnFinish();
+        }
+        text_num.text = curNum.ToString();
+    }
+    public void OnFinish()
+    {
+        playing = false;
+    }
+    public void Play(int Num)
+    {
+        deltaNum = Num - curNum;
+        targetNum = Num;
+        if (deltaNum <= 0)
+            return;
+        if (deltaNum * Time.deltaTime > MaxDuration)
+            changePerDt = (int)(deltaNum * Time.deltaTime / MaxDuration)+1;
+        else
+            changePerDt = 1;
+        playing = true;
+    }
+}
